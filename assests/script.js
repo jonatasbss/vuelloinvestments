8a28f6e2ce472566b42d5986b50aa941621bb43a
Jonatas Batista <jonatasbssampaio@gmail.com>
2023-05-25 17:45:20 -0300

    navbar

--- assests/script.js
+++ assests/script.js
@@ -2,19 +2,20 @@ let backgroundImages = [
     'assests/images/tessa-wilson-MTSILZYVcdw-unsplash.jpg',
     'assests/images/douglas-sheppard-OI3BVhWoli0-unsplash.jpg',
     'assests/images/adam-winger-Bir1x1stKMM-unsplash.jpg',
-    'assests/images/sieuwert-otterloo-aren8nutd1Q-unsplash.jpg'
+    'assests/images/sieuwert-otterloo-aren8nutd1Q-unsplash.jpg',
+    'assests/images/sidekix-media-SL6bC1IVT90-unsplash.jpg'
 ];
 
 let currentImageIndex = 0;
 
-function changeBackgroundImage () {
+function changeBackgroundImage() {
     let body = document.querySelector('body.wrapper')
     body.style.backgroundImage = "url(" + backgroundImages[currentImageIndex] + ")";
 
     currentImageIndex++;
 
 
-    if (currentImageIndex >= backgroundImages.length){
+    if (currentImageIndex >= backgroundImages.length) {
         currentImageIndex = 0;
     }
 }
