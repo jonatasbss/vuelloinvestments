f9612414a5326c359cb7479c79a34a3dcd9b7c07
Jonatas Batista <jonatasbssampaio@gmail.com>
2023-05-25 16:14:27 -0300

    start

---
+++ assests/script.js
@@ -0,0 +1,24 @@
+let backgroundImages = [
+    'assests/images/tessa-wilson-MTSILZYVcdw-unsplash.jpg',
+    'assests/images/douglas-sheppard-OI3BVhWoli0-unsplash.jpg',
+    'assests/images/adam-winger-Bir1x1stKMM-unsplash.jpg',
+    'assests/images/sieuwert-otterloo-aren8nutd1Q-unsplash.jpg'
+];
+
+let currentImageIndex = 0;
+
+function changeBackgroundImage () {
+    let body = document.querySelector('body.wrapper')
+    body.style.backgroundImage = "url(" + backgroundImages[currentImageIndex] + ")";
+
+    currentImageIndex++;
+
+
+    if (currentImageIndex >= backgroundImages.length){
+        currentImageIndex = 0;
+    }
+}
+
+changeBackgroundImage();
+
+setInterval(changeBackgroundImage, 5000);
